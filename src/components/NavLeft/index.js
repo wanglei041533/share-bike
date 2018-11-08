1f171570c0d05b812c69a1ce183f238b37f6167e
wanglei041533 <396844151@qq.com>
2018-11-08 14:51:41 +0800

    navleft动态渲染，header布局，日期格式函数封装，百度天气借口promise封装

--- src/components/NavLeft/index.js
+++ src/components/NavLeft/index.js
@@ -1,24 +1,51 @@
 import React from 'react'
 import { Menu, Icon } from 'antd';
+import menuConfig from '../../config/menuConfig'
+import './index.less'
 
 const SubMenu = Menu.SubMenu;
 
 export default class NavLeft extends React.Component{
+    constructor(props){
+        super(props);
+
+        this.renderMenu =  this.renderMenu.bind(this)
+    }
+
+    componentWillMount(){
+        const menuTreeNode = this.renderMenu(menuConfig)
+        this.setState({
+            menuTreeNode
+        })
+    }
+
+    // 菜单渲染
+    renderMenu(data){
+        return data.map((item)=>{
+            if(item.children){
+                return (
+                    <SubMenu title={item.title} key={item.key}>
+                        { this.renderMenu(item.children) }
+                    </SubMenu>
+                )
+            }
+            return <Menu.Item key={item.key}>{ item.title }</Menu.Item>
+
+        })
+    }
+
     render(){
         return(
             <div className="nav-left">
-                <div>
-                    <img src="" alt=""/>
-                    <h1></h1>
+                <div className="logo">
+                    <img src="/assets/logo-ant.svg" alt=""/>
+                    <h1>wl ms</h1>
                 </div>
                 <div>
                     <Menu
                         theme="dark"
                     >
-                        <SubMenu title={<span><Icon type="mail" /><span>Navigation One</span></span>}>
-                            <Menu.Item key="1">Option 1</Menu.Item>
-                            <Menu.Item key="2">Option 2</Menu.Item>
-                        </SubMenu>
+                        { this.state.menuTreeNode }
                     </Menu>
                 </div>
             </div>
